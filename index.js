44df62ccafc160db7fcdd3f54cd38e8f07a26c13
Arturo Negreiros <anegreiross@ooutlook.com>
2020-03-27 19:31:15 -0500

    ok

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-import { createPopper } from '@popperjs/core';
+//import { createPopper } from 'node_modules/@popperjs/core';
+
+var button = $('#BtnTest');
+var div = $('#DivTest');
+
+div.hide();
+
+button.click(function() {
+
+    var popper = new Popper(button, div, {
+        placement: 'top'
+    });
+
+
+})
+
+
+
+
+
+/*createPopper(button, div, {
+    placement: 'right',
+});*/
+/*(function() {
+    var Undefined = 'Prueba de IFFE';
+    console.log(Undefined);
+})();*/
